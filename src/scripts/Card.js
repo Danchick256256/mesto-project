22db09d7895b67a3cefb5eb8bcbfe75724d2f0a7
Danchick256256 <danilfpronkin280504@gmail.com>
2023-10-07 01:34:13 +0300

    new changes

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -24,44 +24,38 @@ class Card {
             .content.querySelector('.card')
             .cloneNode(true);
 
-        this.cardImage = cardElement.querySelector(".card__image");
-        this.cardImage.src = this.image;
-        this.cardImage.alt = this.name;
-
-        this.cardTitle = cardElement.querySelector(".card__title");
-        this.cardTitle.textContent = this.name;
-
-        this.likeCounter = cardElement.querySelector(".cards__like-counter");
-        this.likeCounter.textContent = this.likes.length === 0 ? "" : this.likes.length;
-
-        this.likeButton = cardElement.querySelector('.card__button-like');
+        this.#setElements(cardElement);
 
         for (const like of this.likes) {
             if (like._id === this.userData._id) {
-                this.likeButton.classList.add('card__button-like_active_true')
+                this.likeButton.classList.add('card__button-like_active_true');
             }
         }
 
-        this.likeButton.addEventListener('click', () => {
-            console.log(`{handled.like.click}`);
-            if (!this.likeButton.classList.contains('card__button-like_active_true')) {
-                api.dislikeCard(this._id)
-                    .then(response => {
-                        this.likeButton.classList.toggle('card__button-like_active_true')
-                        this.likeCounter.textContent = response.likes.length;
-                    })
-                    .catch(err => console.log(err));
-            } else {
-                api.likeCard(this._id)
-                    .then(response => {
-                        this.likeButton.classList.toggle('card__button-like_active_true')
-                        this.likeCounter.textContent = response.likes.length;
-                    })
-                    .catch(err => console.log(err));
-            }
-        });
+        this.cardImage.src = this.image;
+        this.cardImage.alt = this.name;
+
+        this.cardTitle.textContent = this.name;
+
+        this.likeCounter.textContent = this.likes.length === 0 ? "" : this.likes.length;
+
+        this.#setEventListeners(api, cardElement);
+        return cardElement;
+    }
+
+    #setElements(cardElement) {
+        this.cardImage = cardElement.querySelector(".card__image");
+
+        this.cardTitle = cardElement.querySelector(".card__title");
+
+        this.likeCounter = cardElement.querySelector(".cards__like-counter");
+
+        this.likeButton = cardElement.querySelector('.card__button-like');
 
         this.deleteButton = cardElement.querySelector('.card__button-delete');
+    }
+
+    #setEventListeners(api, cardElement) {
         this.deleteButton.addEventListener('click', () => {
             console.log(`{handled.delete.click}`);
             api.deleteCard(this._id)
@@ -88,7 +82,26 @@ class Card {
             openPopup(imagePopup);
         });
 
-        return cardElement;
+        this.likeButton.addEventListener('click', () => {
+            console.log(`{handled.like.click}`);
+            if (this.likeButton.classList.contains('card__button-like_active_true')) {
+                api.dislikeCard(this._id)
+                    .then(response => {
+                        this.likeButton.classList.toggle('card__button-like_active_true')
+                        if (response.likes.length === 0) this.likeCounter.textContent = "";
+                        else this.likeCounter.textContent = response.likes.length;
+                    })
+                    .catch(err => console.log(err));
+            } else {
+                api.likeCard(this._id)
+                    .then(response => {
+                        this.likeButton.classList.toggle('card__button-like_active_true')
+                        if (response.likes.length === 0) this.likeCounter.textContent = "";
+                        else this.likeCounter.textContent = response.likes.length;
+                    })
+                    .catch(err => console.log(err));
+            }
+        });
     }
 }
 
